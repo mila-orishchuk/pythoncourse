d3f4b1d99f23b526100b427d4cf8814fd4412831
mila-orishchuk <mi.otishchuk@gmail.com>
2020-12-17 20:48:35 +0200

    task3

--- task3.py
+++ task3.py
@@ -16,9 +16,32 @@ def letters(symbol = "#"):
     print(symbol, symbol, sep = '\t', end = '\n')
     print(symbol*9, end = '\n')
     print(symbol, symbol, sep = '\t', end = '\n')
-    print(symbol, symbol, sep = '\t', end = '\n')
+    print(symbol, symbol, sep = '\t', end = '\n\n')
 
 letters()
 
-'''
+'''Use loop'''
 
+def litera():
+
+    '''Letter O'''
+
+    i = 0
+    while i < 5:
+        if 0 < i < 4:
+            print("#","#", sep="\t")
+        else:
+            print(9*"#")
+        i += 1
+    print(end = "\n")
+    
+    '''Letter H'''
+    i = 0
+    while i < 5:
+        if i == 2:
+            print (9*"#")
+        else:
+            print("#","#", sep="\t")
+        i += 1
+
+litera()   
