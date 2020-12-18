0db28cde6c7d6f7850374793f68351174fcc021a
mila-orishchuk <mi.otishchuk@gmail.com>
2020-12-18 16:04:59 +0200

    Task2

---
+++ Lesson1/task2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+'''hello world'''
+
+print("hello world")
+
+'''numbers'''
+
+print(1,2,3,4,5,6,7,8,9,10, sep = "!")
+
+'''Enter mail'''
+
+print("Enter mail: ", end = "__________\n")
+
+'''string'''
+
+print("grass", "yellow", "bash", 1.0, 23, "False", 7.5, sep = ", ", end = "\n")
+
+'Beetroot Academy'
+'Another line of code, which will be executed by python interpreter'
