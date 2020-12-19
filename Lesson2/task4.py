83b2f54fe38284528c180bef2cee296212e46e21
mila-orishchuk <mi.otishchuk@gmail.com>
2020-12-19 08:11:57 +0200

    change task

--- Lesson2/task4.py
+++ Lesson2/task4.py
@@ -18,7 +18,8 @@ word = (word.upper() + word.lower() + word.replace('', ' '))
 print(word)
 
 s1 = "Корован"
-s1 = s1.replace('К', 'в')
-s1 = s1[:4] + 'н' + s1[5:6]
-print(s1)
+'''s1 = s1.replace('К', 'в')
+s1 = s1[:4] + 'н' + s1[5:6]'''
+print(s1[4:0:-1] + s1[6:4:-1])
+
 
