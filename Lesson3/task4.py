02d8aa53ab5441d9a3d3ec29bcc23576fba26370
mila-orishchuk <mi.otishchuk@gmail.com>
2020-12-21 18:00:35 +0200

    change first task

--- Lesson3/task4.py
+++ Lesson3/task4.py
@@ -6,16 +6,48 @@ TASK4
 '''
 
 '''
-random 
+Phone number
+
+'''
+
+def validNumber():
+
+    phone_number = input('Enter your phone number:')
+    phone_number = phone_number.replace(' ', '').replace('+38', '')
+    operators = ['063', '093', '050']
+    
+    if not phone_number.isnumeric():
+        print('Phone number must contian only numbers')
+
+    elif len(phone_number) != 10: 
+        print('Wrong length number')
+
+    elif phone_number[0:3] not in operators:
+        print('Unknown operator')
+
+    else:
+        print('Number is correct')
+        
+if __name__ == '__main__':
+    validNumber()
+
+
+
+
+
+
+'''
+random ariphmetic
 
 '''
 
 import random
 import operator
 
-def random_math(
-    a = random.randint(1,20),
-    b = random.randint(1,20)):    
+def random_math():   
+
+    a = random.randint(1,20)
+    b = random.randint(1,20) 
 
     operators = [operator.add, operator.sub, operator.mul, operator.truediv]
     random_operator = random.choice(operators)
@@ -42,10 +74,10 @@ def IsPalindrome():
     rev_string = string[::-1]
 
     if string == rev_string:
-        print("yes")
+        print("palindrome")
 
     else:
-        print("no")
+        print("isn`t palindrome")
 
 if __name__ == '__main__':
     IsPalindrome()
