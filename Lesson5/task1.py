eb35d0ea7f6f5a2c7f478d766c78c75e5f8a9f18
mila-orishchuk <mi.otishchuk@gmail.com>
2020-12-25 20:31:28 +0200

    add one-line solution for 'for in'

--- Lesson5/task1.py
+++ Lesson5/task1.py
@@ -7,6 +7,7 @@ The greatest number
 import random
 
 def generate_random_list(length = 10, start = 1, end = 100):
+#   return [random.randint(start,end) for i in range(0, length)]
     random_list = []
 
     for i in range(0, length):
