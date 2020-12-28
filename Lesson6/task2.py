c57ed534e4cb01daa5334562454398fbee03b8b3
mila-orishchuk <mi.otishchuk@gmail.com>
2020-12-28 19:36:44 +0200

    add func

--- Lesson6/task2.py
+++ Lesson6/task2.py
@@ -4,25 +4,29 @@
 Create a function which takes as input two dicts with structure mentioned above,
 then computes and returns the total price of stock.
 '''
+def total_price(stock, prices):
+	total_price = 0
 
-stock = {
-    "banana": 6,
-    "apple": 0,
-    "orange": 32,
-    "pear": 15
-}
+	for key in stock:
+		if key in prices:
+			total_price += stock[key] * prices[key]
+	return total_price
 
-prices = {
-    "banana": 4,
-    "apple": 2,
-    "orange": 1.5,
-    "pear": 3
-}
+if __name__ == '__main__':
+    stock = {
+        "banana": 6,
+        "apple": 0,
+        "orange": 32,
+        "pear": 15
+    }
+    prices = {
+        "banana": 4,
+        "apple": 2,
+        "orange": 1.5,
+        "pear": 3
+    }
 
-total_price = {}
+    tot_price = total_price(stock, prices)
+    print(tot_price)
 
-for key, value in stock.items():
-    total_price[key] = stock[key] * prices[key]
-    total_price_stock = sum(total_price.values())
     
-print(f'Total price of each fruit is: {total_price} and total price of stock: {total_price_stock}')
