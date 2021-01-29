f7b8336f9248eb68bce5353ded5d258d0192fed6
mila-orishchuk <mi.otishchuk@gmail.com>
2021-01-29 14:08:54 +0200

    fix generator

--- Lesson16/task2.py
+++ Lesson16/task2.py
@@ -6,24 +6,35 @@ Tips: See the documentation for `range` function
 '''
 
 
-def in_range(start, end=None, step=1):
+def in_range(start: int, end = None, step = 1):
+    if not all((arg, int) for arg in (start, end, step)):
+       raise TypeError
+    
     if end is None:
         start, end = 0, start
 
-    if start > end:
-        return
-
+    if step == 0:
+        raise ValueError('Does not accept step = 0')
     current = start
-    while current < end:
+
+    while (step > 0 and current < end or step < 0 and current > end):
         yield current
         current += step
-
-
+    
 if __name__ == '__main__':
     data_sets = [
         (10,),
-        (21, 11),
-        (-10, 10, 2)
+        (21, 10),
+        (10, -1, -2),
+        (-10, 10, 2),
+        (1, 20, 0),
+        (2, 10, 2.5)
     ]
     for data_set in data_sets:
-        assert list(in_range(*data_set)) == list(range(*data_set))
+        try:
+            assert list(in_range(*data_set)) == list(range(*data_set))
+        except ValueError as error:
+            print(error)
+        except TypeError as error:
+            print(error)
+
