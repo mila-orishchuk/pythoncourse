f8fb8c384bddfefbdaa8fa2fe52f8073e66a5640
mila-orishchuk <mi.otishchuk@gmail.com>
2021-01-31 11:00:35 +0200

    fix code

--- Lesson16/task2.py
+++ Lesson16/task2.py
@@ -7,17 +7,17 @@ Tips: See the documentation for `range` function
 
 
 def in_range(start: int, end=None, step=1):
-    if not all((arg, int) for arg in (start, end, step)):
-        raise TypeError
-
     if end is None:
         start, end = 0, start
+   
+    if not all(isinstance(arg, int) for arg in (start, end, step)):
+        raise TypeError('All arguments must be integer.')
 
     if step == 0:
         raise ValueError('Does not accept step = 0')
 
     current = start
-    while (step > 0 and current < end or step < 0 and current > end):
+    while (step > 0 and current < end) or (step < 0 and current > end):
         yield current
         current += step
 
@@ -29,12 +29,15 @@ if __name__ == '__main__':
         (10, -1, -2),
         (-10, 10, 2),
         (1, 20, 0),
-        (2, 10, 2.5)
+        (2, 10, 2.5),
+        (2, 10, True)
     ]
     for data_set in data_sets:
         try:
             assert list(in_range(*data_set)) == list(range(*data_set))
         except ValueError as error:
+            print(data_set)
             print(error)
         except TypeError as error:
+            print(data_set)
             print(error)
