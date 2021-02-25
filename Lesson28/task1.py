4e59ccfd5112b8cfb48748feb9853db554c29429
mila-orishchuk <mi.otishchuk@gmail.com>
2021-02-25 22:20:00 +0200

    Дерева та алгоритми дерев

---
+++ Lesson28/task1.py
@@ -0,0 +1,77 @@
+'''
+Implement a binary heap as a max heap
+'''
+
+
+class MaxHeap:
+    def __init__(self, alist=None):
+        self.heap = [0]
+        self.current_size = 0
+        if alist is not None:
+            self.create_max_heap(alist)
+            self.heap = alist
+            self.current_size = len(alist)
+
+    def create_max_heap(self, alist):
+        n = len(alist)
+
+        for i in range(int(n / 2), -1, -1):
+            self.max_heapify(i, alist, n)
+
+    def max_heapify(self, indx, alist, size):
+        left_child = indx * 2
+        right_child = indx * 2 + 1
+        largest = indx
+
+        if left_child < size:
+            if alist[left_child] > alist[largest]:
+                largest = left_child
+        if right_child < size:
+            if alist[right_child] > alist[largest]:
+                largest = right_child
+
+        if largest != indx:
+            alist[indx], alist[largest] = alist[largest], alist[indx]
+            self.max_heapify(largest, alist, size)
+
+    def insert(self, value):
+        self.heap.append(value)
+        self.current_size += 1
+
+        indx = self.current_size - 1
+
+        parent = int(indx / 2 - 1)
+
+        while parent >= 0 and self.heap[indx] > self.heap[parent]:
+            self.heap[indx], self.heap[parent] = self.heap[parent], self.heap[indx]
+            indx = parent
+            parent = int(indx / 2 - 1)
+
+    def delete(self, indx):
+        if self.current_size == 0:
+            print("Heap Underflow!!")
+            return
+
+        self.heap[-1], self.heap[indx] = self.heap[indx], self.heap[-1]
+        self.current_size -= 1
+
+        self.max_heapify(indx, self.heap, self.current_size)
+
+        return self.heap.pop()
+
+    def extract_max(self):
+        return self.delete(0)
+
+    def __str__(self):
+        print(*self.heap)
+
+
+if __name__ == "__main__":
+    heap = MaxHeap([5, 10, 4, 8, 3, 0, 9, 11])
+    heap.__str__()
+    heap.insert(15)
+    heap.__str__()
+    print(heap.delete(2))
+    heap.__str__()
+    print(heap.extract_max())
+    heap.__str__()
---
+++ Lesson28/task2.py
@@ -0,0 +1,8 @@
+'''
+Using the BinaryHeap class, implement a new class called PriorityQueue.
+Your PriorityQueue class should implement the constructor, plus the enqueue and dequeue methods.
+'''
+
+
+if __name__ == "__main__":
+    pass
