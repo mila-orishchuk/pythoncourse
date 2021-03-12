a00b1f993bb1d004ec6f3d42f88116d9e5140295
mila-orishchuk <mi.otishchuk@gmail.com>
2021-03-12 21:37:01 +0200

    fixing code

--- Lesson34/task2.py
+++ Lesson34/task2.py
@@ -8,44 +8,56 @@ and dump it to a file. For this task use concurrent and multiprocessing
 libraries for making requests to Reddit API.
 """
 
-
 import requests
 import json
-from multiprocessing import Process
+from multiprocessing import Process, Manager
 import concurrent
 import datetime
-import time
-import logging
+from concurrent.futures import ProcessPoolExecutor
 
-
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-logging.basicConfig(format="[%(asctime)s] [%(levelname)s] {%(filename)s:%(lineno)d} {%(process)d:%(threadName)s} - %(message)s")
-
+manager = Manager()
+shared_list = manager.list()
 
 
 class CommentsDownloader(Process):
-    def __init__(self, author, parameters):
-        super().__init__()
-        self._author = author
-        self._parameters = parameters
+    def __init__(self, subreddit):
+        super().__init__(target=self.run)
+        self._parameters = {'subreddit': subreddit}
+
+    def start(self):
+        print('process start')
+        super().start()
 
     def run(self):
         response = requests.get(URL, params=self._parameters)
         if response.ok:
-            with open(FILE, 'a+') as file:
-                json.dump(response.json(), file, indent=4)
-                file.write(',\n')
+            global shared_list
+            shared_list.append(response.json())
         else:
-            raise Exception
+            raise Exception('something went wrong')
+
+    def sort_shared_list(self):
+        pass
+
+
+def create_comments_downloader(subreddit: str):
+    process = CommentsDownloader(subreddit)
+    process.start()
 
 
 if __name__ == '__main__':
+
     FILE = 'comments.json'
     URL = "https://api.pushshift.io/reddit/comment/search/"
-    
-    for author in author_list:
-        params = {'size': 5, 'author': author, 'fields': ('author', 'body', 'created_utc', 'subreddit')}
-        thread = CommentsDownloader(author, params)
-        thread.start()
-        thread.join()
+    subreddits = ['politics', 'teenagers']
+    all_comments = []
+    with ProcessPoolExecutor() as executor:
+        executor.map(create_comments_downloader, subreddits, timeout=60)
+        executor.shutdown()
+        for subreddit_comments in shared_list:
+            for comment in subreddit_comments['data']:
+                all_comments.append(comment)
+        all_comments.sort(key=lambda comment: comment['created_utc'])
+        
+        with open(FILE, 'w') as f:
+            f.write(json.dumps(all_comments))
